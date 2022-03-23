30cbe18b2c22a2442125fa1fc99174b8d2e2aa80
Psevdonim <Egorka-su@mail.ru>
2022-03-23 21:06:33 +0300

    ff

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -47,6 +47,13 @@ toTopButton.addEventListener('click', () => {
 	})
 });
 
+window.onload = function () {
+	document.body.classList.add('loaded_hiding');
+	window.setTimeout(function () {
+		document.body.classList.add('loaded');
+		document.body.classList.remove('loaded_hiding');
+	}, 500);
+}
 
 
 function animateValue(element) {
